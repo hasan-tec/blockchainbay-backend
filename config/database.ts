f285a482dac7c23ad4dc2822a4cc42e37ff7a53e
hasan-tec <131611130+hasan-tec@users.noreply.github.com>
2025-03-17 17:28:22 +0500

    Update database.ts

--- config/database.ts
+++ config/database.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 
 export default ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres'); // Default to postgres
+  const client = env('DATABASE_CLIENT', 'postgres');
 
   const connections = {
     mysql: {
@@ -24,13 +24,15 @@ export default ({ env }) => {
     },
     postgres: {
       connection: {
-        connectionString: env('DATABASE_URL'), // Optional: Use if provided by Render
+        connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST', 'dpg-cvc12p5umphs73cfidh0-a'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'blockchainbay_chris'),
         user: env('DATABASE_USERNAME', 'blockchainbay_chris_user'),
         password: env('DATABASE_PASSWORD', 'z4UecT4u4wTTUJM14cewPyEPBX8Hb0adpg-cvc12p5umphs73cfidh0-a'),
-        ssl: env.bool('DATABASE_SSL', true), // Render requires SSL for external connections
+        ssl: env.bool('DATABASE_SSL', true) && {
+          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false), // Allow self-signed certificates
+        },
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
